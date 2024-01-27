4ccdd2bcec7f9d4a3ed09ab045126ceabb42f1ca
Joe Gonzales <joegonzalesw6@gmail.com>
2024-01-27 01:57:19 -0600

    update about me

--- src/App.js
+++ src/App.js
@@ -11,6 +11,16 @@ function App() {
       <div className="App">
         <CardComponent>
           <NameField name="Joseph Gonzales" />
+          <p>
+            Hello! You can call me Joe if you'd like.
+          </p>
+          <p>
+            I'm a 4th year computer science student at the University of Manitoba.
+            I'm also a big sports guy, I enjoy practicing yoga, and I love checking out new restaurants.
+          </p>
+          <p>
+            Thanks for stopping by!
+          </p>
           <Navigation />
         </CardComponent>
       </div>
